8e5f82d57f95aee11e01020d98ac0851ed5096c0
Leonardo Amorim Ramos <leoamorimr@gmail.com>
2021-04-05 10:58:49 -0300

    Ajuste para busca do pet pelo sobrenome do sono

--- src/main/java/br/com/leoamorimr/pethealthy/pethealthybackend/repository/PetRepository.java
+++ src/main/java/br/com/leoamorimr/pethealthy/pethealthybackend/repository/PetRepository.java
@@ -11,7 +11,7 @@ import br.com.leoamorimr.pethealthy.pethealthybackend.model.Pet;
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
-    @Query(value = "SELECT p FROM Pet p INNER JOIN Person pe ON(p.person.id = pe.id) WHERE pe.first_name LIKE %?1%")
+    @Query(value = "SELECT p FROM Pet p INNER JOIN Person pe ON(p.person.id = pe.id) WHERE (pe.first_name LIKE %?1% OR pe.last_name LIKE %?1%)")
     List<Pet> findByOwnerFirstName(String first_name);
 
 
